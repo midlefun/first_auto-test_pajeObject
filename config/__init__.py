73358cd0c8c44ab68d33a02feddf9f9db7f6814a
midlefun <vladik1997s60@gmail.com>
2024-01-29 00:05:28 +0300

    change config. ADD fixtures

---
+++ config/__init__.py
@@ -0,0 +1,7 @@
+from config.expectations import Expectations
+from config.playwright import Playwright
+from config.url import Url
+
+url = Url()
+playwright = Playwright()
+expectations = Expectations()
